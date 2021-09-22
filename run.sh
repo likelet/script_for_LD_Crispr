a124055a71e51ea3b901eb2c0cedae53257a7566
likelet <zhaoqi699@sina.com>
2021-09-22 21:16:45 +0800

    add merge code

--- run.sh
+++ run.sh
@@ -2,3 +2,9 @@
 cat sample.txt | xargs -iF -P2 sh -c "sh count.sh F"
 
 ls *.count |xargs -iF -P1 sh -c "Rscript normalized.R F lib.annotate_update.tsv"
+
+# combine result 
+
+ls *.csv | rush 'csvtk rename -f normalized -n {.} {} -o {}.csv2'
+
+csvtk join --outer-join --na 0 -f 1-4 --infile-list <(ls *.csv2) > merge.csv
